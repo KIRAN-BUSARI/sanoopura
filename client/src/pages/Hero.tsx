7920c737bd6312787aa3983896275b8622c1b2dd
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-07-21 15:23:58 +0530

    Add Hero Section

---
+++ client/src/pages/Hero.tsx
@@ -0,0 +1,30 @@
+export default function Hero() {
+  return (
+    <div className="h-auto bg-gradient-to-br from-[#26172E] via=[#34203E] to-[#3D2349]">
+      <div className="grid sm:grid-cols-12 gap-4 px-[120px] items-center">
+        <div className="col-span-6 text-white flex flex-col justify-center">
+          <h1 className="text-5xl leading-[150%] text-[#E2B398] font-medium">
+            Dr. Archana S Rao
+          </h1>
+          <p className="text-lg text-[#E2B398]">
+            Bharatha Siri, Karnataka Siri, Uttama Nruthyagarthi Kishora
+            Prathibhe and Janapada Siri,
+          </p>
+          <div className="mt-8">
+            <button className="px-6 py-2 bg-[#E2B398] text-[#34203E] rounded-full">
+              Know More
+            </button>
+          </div>
+        </div>
+        <div className="col-span-6 z-0 flex justify-center relative overflow-hidden">
+          <img
+            src="/src/assets/image.png"
+            alt="dancer"
+            className="h-auto w-auto object-contain"
+          />
+          <div className="absolute -z-10 inset-0 bg-center bg-no-repeat bg-heroImage opacity-50 h-[80vh] animate-spin-slow"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
