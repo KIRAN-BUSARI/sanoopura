4a657280336947d42e3cad27704600091877bc09
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-07-25 00:09:13 +0530

    Add logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,10 +1,5 @@
+import logo from "../assets/Logo.svg";
 const Logo = () => {
-  return (
-    <img
-      src="https://image-resource.creatie.ai/125443918377437/125443918377439/713ed9b4a495f05c2195f5494c1039b5.png"
-      className={"image"}
-      alt="Logo"
-    />
-  );
+  return <img src={logo} className={"image"} alt="Logo" />;
 };
 export default Logo;
