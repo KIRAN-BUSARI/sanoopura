4b0ef1b8c765a29e7a7009997d72d8fdc2027329
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-09-11 12:05:37 +0530

    Fix banner for mobile

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -2,11 +2,13 @@ const Banner = () => {
   return (
     <div className="w-full h-auto bg-secondary flex place-content-center text-white py-[30px] z-10">
       <div className="flex flex-col justify-center items-center space-y-2">
-        <h1 className="text-2xl text-center">Bharatanatyam Classes</h1>
-        <p className="text-justify text-base font-light">
-          Admissions for next academic are open till end of the JUNE 2024
+        <h1 className="sm:text-2xl text-xl text-center">
+          Bharatanatyam Classes
+        </h1>
+        <p className="sm:text-base text-xs font-light text-center text-pretty">
+          Admissions for next academic are open till end of the JUNE 2024 <br />
+          Beginners - Intermediate - Advanced
         </p>
-        <p className="font-light">Beginners - Intermediate - Advanced</p>
       </div>
     </div>
   );
